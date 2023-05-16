0c7d0e2b874c7811bc0451c59754a4d4ba4992dc
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2023-05-16 18:06:00 +0500

    Created react project

---
+++ src/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <div>
+        <input type='text' placeholder='Total Budget'></input>
+        <button >Add Expence</button>
+    </div>
+  )
+}
+
+export default MainPage
